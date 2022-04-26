0c8793cf90441a8aa79d39426700312ee9891c43
vdhakar80 <vdhakar80@gmail.com>
2022-04-26 08:38:16 +0530

    second commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCAvEf4yEpnUJ6nyUb7Nwj2r-Ll80pC1so",
-  authDomain: "chat-app-3d58d.firebaseapp.com",
-  projectId: "chat-app-3d58d",
-  storageBucket: "chat-app-3d58d.appspot.com",
-  messagingSenderId: "472500071541",
-  appId: "1:472500071541:web:cfd0775abf504d804e9c3c",
-  measurementId: "G-8XV89L03T8",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
